bc278336ba469391a3610f25bf6e5b479547a71f
Aschellevis <302953884@student.rocmondriaan.nl>
2022-12-08 12:00:25 +0100

    Chatbot afgemaakt.

--- ChatBot/ChatBot/ConversationDataSource.swift
+++ ChatBot/ChatBot/ConversationDataSource.swift
@@ -2,21 +2,39 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    let messageCount = 0
+    var messageCount = 0
+    
+    var messages = [Message]()
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
+        var messageCount: Int {
+            return messages.count
+        }
+        let message = Message(date: Date(), text:
+                                question, type: .question)
+        messages.append(message)
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
+        var messageCount: Int {
+            return messages.count
+        }
+        let message = Message(date: Date(), text:
+                                answer, type: .answer)
+        messages.append(message)
     }
     
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
-        return Message(date: Date(), text: "Hello, world!", type: .answer)
+        if index % 2 == 0 {
+            return messages[index]
+        } else {
+            return messages[index]
+        }
     }
 }
